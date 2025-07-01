cc3edc1e1ddb46147aed6b603d8617834da2b55b
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-07-01 11:53:13 +0530

    Update data structures.py

--- data structures.py
+++ data structures.py
@@ -1,4 +1,4 @@
-arr=[10,20,30,40]
+'''arr=[10,20,30,40]
 arr.append(50)
 arr.insert(2,25)
 print(arr)
@@ -8,4 +8,35 @@ arr.pop()
 for i in arr:
     print(i,end=" ")
 print("\n 25 i array?", 25 in arr)
-https://my-portfolio-project-lime-iota.vercel.app/
+
+
+
+text=input("enter a name :")
+if text==text[::-1]:
+    print("palindrome")
+else:
+    print("not")
+freq={}
+for ch in text:
+    freq[ch]=freq.get(ch,0)+1
+
+print(req)'''
+
+
+def linear search(arr, key):
+    for i in range(len(arr)):
+      if arr[i]==key:
+        return i
+    return -1
+size= int(input("Enter the size of array:"))
+arr=[]
+print("Enter the elements:")
+I
+for i in range(size):
+   num= int(input(f"Element \{i + 1\} :"))
+   arr.append(num)
+key = int(input("Enter the element to search:"))
+result linear_search(arr, key)
+
+if result != -1:
+   print(f"\n eleent")
